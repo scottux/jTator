c7a179e7e58b2aa86409a3bdab916d70c1abc701
Scott <d.scott.carroll@gmail.com>
2012-05-22 00:50:35 -0300

    now fades all direct child elements regardless of type. Also cleaned up fade routine.

--- jQ.jTator.js
+++ jQ.jTator.js
@@ -4,16 +4,19 @@
             'speed' : 4000
         }, options);
         return this.each(function (){
-            var $darthFader = $(this), rotate;
+            var $darthFader = $(this),
+                rotate;
 
-            function fadeNext() {
-                getFirst().stop(true, true).fadeOut().appendTo($darthFader);
-                getFirst().stop(true, true).fadeIn();
-            }
             function getFirst(){
-                return $darthFader.find('img:first');
+                return $darthFader.find('>:first');
             }
-            $darthFader.addClass('jTator').find('img:not(:first)').hide();
+            function fadeNext() {
+                getFirst().fadeOut("fast", function (){
+                    $(this).appendTo($darthFader);
+                    getFirst().fadeIn(options.speed/2);
+                });
+            }
+            $darthFader.addClass('jTator').find('>:not(:first)').hide();
             rotate = setInterval(fadeNext, options.speed);
         });
     };
